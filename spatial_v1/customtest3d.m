0d70a43caae99b9d264d1fa11e4f5d7cccf04a63
Martin Felis <martin@fysx.org>
2010-08-31 15:16:50 +0200

    added customization to spatial_v1

---
+++ spatial_v1/customtest3d.m
@@ -0,0 +1,46 @@
+function qdd = customtest3d(nb)
+
+if nargin < 1
+	nb = 3
+end
+
+bf = 0;
+skew = 0;
+taper = 0;
+
+model.NB = nb;
+model.pitch = zeros(1,model.NB);
+
+% 1st body
+model.parent(1) = 0;
+model.Xtree{1} = Xtrans([0 0 0]);
+model.jaxis{1} = [0 0 1]';
+mass = 1.;
+CoM = [1 0 0];
+Icm = diag([1 1 1]);
+model.I{1} = mcI(mass, CoM, Icm);
+
+model.parent(2) = 1;
+model.Xtree{2} = Xtrans([1 0 0]);
+model.jaxis{2} = [0 1 0]';
+mass = 1.;
+CoM = [0 1 0];
+Icm = diag([1 1 1]);
+model.I{2} = mcI(mass, CoM, Icm);
+
+model.parent(3) = 2;
+model.Xtree{3} = Xtrans([0 1 0]);
+model.jaxis{3} = [1 0 0]';
+mass = 1.;
+CoM = [0 0 1];
+Icm = diag([1 1 1]);
+model.I{3} = mcI(mass, CoM, Icm);
+
+q = zeros(nb,1);
+qd = zeros(nb,1);
+tau = zeros(nb,1);
+
+%q(1) = pi * 0.5;
+%qd(1) = 10;
+
+qdd = FDab (model, q, qd, tau, {}, [0; -9.81; 0]);
--- spatial_v1/jcalc.m
+++ spatial_v1/jcalc.m
@@ -1,4 +1,4 @@
-function  [Xj,S] = jcalc( pitch, q )
+function  [Xj,S] = jcalc( pitch, q, axis )
 
 % jcalc  Calculate joint transform and motion subspace.
 % [Xj,S]=jcalc(pitch,q) calculates the joint transform and motion subspace
@@ -6,9 +6,29 @@ function  [Xj,S] = jcalc( pitch, q )
 % (pitch==any other value) joint.  For revolute and helical joints, q is
 % the joint angle.  For prismatic joints, q is the linear displacement.
 
+if nargin < 3
+	axis = [0;0;1];
+end
+
+% type information:
+% 0 rotz
+% 1 roty
+% 2 rotx
+
 if pitch == 0				% revolute joint
-  Xj = Xrotz(q);
-  S = [0;0;1;0;0;0];
+	if axis == [0;0;1]
+  	Xj = Xrotz(q);
+  	S = [0;0;1;0;0;0];
+	elseif axis == [0;1;0]
+  	Xj = Xroty(q);
+  	S = [0;1;0;0;0;0];
+	elseif axis == [1;0;0]
+  	Xj = Xrotx(q);
+  	S = [1;0;0;0;0;0];
+	else
+		disp("invalid joint axis!")
+		return
+	end
 elseif pitch == inf			% prismatic joint
   Xj = Xtrans([0 0 q]);
   S = [0;0;0;0;0;1];
