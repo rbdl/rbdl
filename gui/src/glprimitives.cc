2f8ddbca5e378044297b19c7504ebafbe53d1ab2
Martin Felis <martin@fysx.org>
2011-01-07 21:45:49 +0100

    gui: fixed normal vectors on the cube

--- gui/src/glprimitives.cc
+++ gui/src/glprimitives.cc
@@ -12,8 +12,6 @@
 
 enum BufferIndex {
 	BufferIndexCubePosition = 0,
-	BufferIndexCubeColor,
-	BufferIndexCubeIndex,
 	BufferIndexCubeNormal,
 	BufferIndexDiscPosition,
 	BufferIndexDiscNormal,
@@ -39,50 +37,26 @@ static GLsizeiptr torus_normal_size = 0;
 static GLfloat *torus_normal_data = NULL;
 static GLsizei torus_element_count = 0;
 	
-GLsizeiptr cube_position_size  = 8 * 3 * sizeof (GLfloat);
+GLsizeiptr cube_position_size  = 6 * 4 * 3 * sizeof (GLfloat);
 GLfloat cube_position_data[] = {
-	-1.0f, -1.0f,  1.0f,
-	 1.0f, -1.0f,  1.0f,
-	 1.0f,  1.0f,  1.0f,
-	-1.0f,  1.0f,  1.0f,
-	-1.0f, -1.0f, -1.0f,
-	 1.0f, -1.0f, -1.0f,
-	 1.0f,  1.0f, -1.0f,
-	-1.0f,  1.0f, -1.0f
-};
+	 1, 1, 1,  -1, 1, 1,  -1,-1, 1,   1,-1, 1,    // v0-v1-v2-v3
+	 1, 1, 1,   1,-1, 1,   1,-1,-1,   1, 1,-1,    // v0-v3-v4-v5
+	 1, 1, 1,   1, 1,-1,  -1, 1,-1,  -1, 1, 1,    // v0-v5-v6-v1
+	-1, 1, 1,  -1, 1,-1,  -1,-1,-1,  -1,-1, 1,    // v1-v6-v7-v2
+	-1,-1,-1,   1,-1,-1,   1,-1, 1,  -1,-1, 1,    // v7-v4-v3-v2
+	 1,-1,-1,  -1,-1,-1,  -1, 1,-1,   1, 1,-1     // v4-v7-v6-v5
+};   
 
-GLsizeiptr cube_color_size = 8 * 3 * sizeof (GLubyte);
-GLubyte cube_color_data[] = {
-	 27, 227,  27,
-	 27,  27, 227,
-	227,  27,  27,
-	 27, 227, 227,
-	227, 227,  27,
-	227,  27, 227,
-	227, 227, 227,
-	 27,  27,  27
-};
-
-GLsizeiptr cube_index_size = 6 * 4 * sizeof (GLubyte);
-GLubyte cube_index_data[] = {
-	0, 1, 2, 3,
-	6, 2, 1, 5,
-	7, 6, 5, 4,
-	3, 7, 4, 0,
-	3, 2, 6, 7,
-	0, 4, 5, 1
-};
-
-GLsizeiptr cube_normal_size = 6 * 3 * sizeof (GLfloat);
+GLsizeiptr cube_normal_size = 6 * 4 * 3 * sizeof (GLfloat);
 GLfloat cube_normal_data[] = {
-	 0.0f,  0.0f,  1.0f,
-	 1.0f,  0.0f,  0.0f,
-	 0.0f,  0.0f, -1.0f,
-	-1.0f,  0.0f,  0.0f,
-	 0.0f,  1.0f,  0.0f,
-	 0.0f, -1.0f,  0.0f,
-};
+	 0, 0, 1,   0, 0, 1,  0, 0, 1,  0, 0, 1, // v0-v1-v2-v3
+	 1, 0, 0,   1, 0, 0,  1, 0, 0,  1, 0, 0, // v0-v3-v4-v5
+	 0, 1, 0,   0, 1, 0,  0, 1, 0,  0, 1, 0, // v0-v5-v6-v1
+	-1, 0, 0,  -1, 0, 0, -1, 0, 0, -1, 0, 0, // v1-v6-v7-v2
+	 0,-1, 0,   0,-1, 0,  0,-1, 0,  0,-1, 0, // v7-v4-v3-v2
+	 0, 0,-1,   0, 0,-1,  0, 0,-1,  0, 0,-1  // v4-v7-v6-v5
 
+};        
 void check_opengl_error (const char* file, int line) {
 	GLenum gl_error = glGetError();
 	if (gl_error) {
@@ -96,18 +70,6 @@ void glprimitives_cube_init () {
 	glBindBuffer (GL_ARRAY_BUFFER, BufferNames[BufferIndexCubePosition]);
 	check_opengl_error (__FILE__, __LINE__);
 	glBufferData (GL_ARRAY_BUFFER, cube_position_size, cube_position_data, GL_STATIC_DRAW);
-	check_opengl_error (__FILE__, __LINE__);
-
-	// Color
-	glBindBuffer (GL_ARRAY_BUFFER, BufferNames[BufferIndexCubeColor]);
-	check_opengl_error (__FILE__, __LINE__);
-	glBufferData (GL_ARRAY_BUFFER, cube_color_size, cube_color_data, GL_STATIC_DRAW);
-	check_opengl_error (__FILE__, __LINE__);
-
-	// Indexes
-	glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, BufferNames[BufferIndexCubeIndex]);
-	check_opengl_error (__FILE__, __LINE__);
-	glBufferData (GL_ELEMENT_ARRAY_BUFFER, cube_index_size, cube_index_data, GL_STATIC_DRAW);
 	check_opengl_error (__FILE__, __LINE__);
 
 	// Normals
@@ -270,31 +232,24 @@ void glprimitives_cube () {
 	check_opengl_error (__FILE__, __LINE__);
 	glVertexPointer (3, GL_FLOAT, 0, 0);
 	check_opengl_error (__FILE__, __LINE__);
-	glBindBuffer (GL_ARRAY_BUFFER, BufferNames[BufferIndexCubeColor]);
-	check_opengl_error (__FILE__, __LINE__);
-	glColorPointer (3, GL_UNSIGNED_BYTE, 0, 0);
-	check_opengl_error (__FILE__, __LINE__);
-	glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, BufferNames[BufferIndexCubeIndex]);
-	check_opengl_error (__FILE__, __LINE__);
-	glIndexPointer(GL_UNSIGNED_BYTE, 0, (void *) 0);
-
 	glBindBuffer (GL_ARRAY_BUFFER, BufferNames[BufferIndexCubeNormal]);
 	check_opengl_error (__FILE__, __LINE__);
 	glNormalPointer (GL_FLOAT, 0, 0);
 	check_opengl_error (__FILE__, __LINE__);
 
 	glEnable(GL_COLOR_MATERIAL);
-	glColorMaterial (GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+//	glColorMaterial (GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
 	glEnableClientState (GL_VERTEX_ARRAY);
 	glEnableClientState (GL_NORMAL_ARRAY);
-//	glEnableClientState (GL_COLOR_ARRAY);
 
-	glDrawElements(GL_QUADS, 36, GL_UNSIGNED_BYTE, 0);
-//	glDrawArrays (GL_LINES, 0, 8);
+	glEnable(GL_RESCALE_NORMAL);
+
+	glDrawArrays (GL_QUADS, 0, 6 * 3 * 4);
+
+	glDisable(GL_RESCALE_NORMAL);
 
 	glDisableClientState (GL_VERTEX_ARRAY);
 	glDisableClientState (GL_NORMAL_ARRAY);
-//	glDisableClientState (GL_COLOR_ARRAY);
 	glDisable(GL_COLOR_MATERIAL);
 
 	glBindBuffer (GL_ARRAY_BUFFER, 0);
@@ -314,7 +269,11 @@ void glprimitives_disc () {
 	glEnableClientState (GL_VERTEX_ARRAY);
 	glEnableClientState (GL_NORMAL_ARRAY);
 
+	glEnable(GL_RESCALE_NORMAL);
+
 	glDrawArrays (GL_TRIANGLE_FAN, 0, disc_element_count);
+
+	glDisable(GL_RESCALE_NORMAL);
 
 	glDisableClientState (GL_VERTEX_ARRAY);
 	glDisableClientState (GL_NORMAL_ARRAY);
@@ -338,7 +297,11 @@ void glprimitives_torus () {
 	glEnableClientState (GL_VERTEX_ARRAY);
 	glEnableClientState (GL_NORMAL_ARRAY);
 
+	glEnable(GL_RESCALE_NORMAL);
+
 	glDrawArrays (GL_QUAD_STRIP, 0, torus_element_count);
+
+	glDisable(GL_RESCALE_NORMAL);
 
 	glDisableClientState (GL_VERTEX_ARRAY);
 	glDisableClientState (GL_NORMAL_ARRAY);
@@ -346,11 +309,12 @@ void glprimitives_torus () {
 
 	glBindBuffer (GL_ARRAY_BUFFER, 0);
 
+	// Draw the caps (i.e. discs at the open ends)
 	glPushMatrix();
-	glTranslatef (0., 0., 0.5);
-	glprimitives_disc();
-	glTranslatef (0., 0., -1.);
-	glRotatef (180, 0, 1., 0.);
-	glprimitives_disc();
+		glTranslatef (0., 0., 0.5);
+		glprimitives_disc();
+		glTranslatef (0., 0., -1.);
+		glRotatef (180, 0, 1., 0.);
+		glprimitives_disc();
 	glPopMatrix();
 }
