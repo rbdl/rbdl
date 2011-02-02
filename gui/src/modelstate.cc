612a2958aa3e35f64a645563650c25c395d3556a
Martin Felis <martin@fysx.org>
2011-02-02 17:50:34 +0100

    gui: using model from Contact tests and started to implement RK45 integrator

--- gui/src/modelstate.cc
+++ gui/src/modelstate.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <QDebug>
 #include <assert.h>
 
@@ -9,11 +11,22 @@
 #include "Dynamics.h"
 #include "Kinematics.h"
 
+using namespace std;
+
 static Model* model = NULL;
 
 unsigned int body_a_id, body_b_id, body_c_id, body_d_id;
 Body body_a, body_b, body_c, body_d;
 Joint joint_a, joint_b, joint_c, joint_d;
+
+unsigned int base_rot_z_id, base_rot_y_id, base_rot_x_id,
+	child_rot_z_id, child_rot_y_id, child_rot_x_id;
+
+Body base_rot_z, base_rot_y, base_rot_x,
+	child_rot_z, child_rot_y, child_rot_x;
+
+Joint joint_base_rot_z, joint_base_rot_y, joint_base_rot_x,
+	joint_child_rot_z, joint_child_rot_y, joint_child_rot_x;
 
 cmlVector Q;
 cmlVector QDot;
@@ -26,20 +39,109 @@ Vector3d contact_normal;
 
 typedef cmlVector (rhs_func) (double, const cmlVector&);
 
-cmlVector rk4_integrator (double t, double h, cmlVector &y0, rhs_func func) {
+cmlVector rk45_integrator (double t0, double tf, cmlVector &y0, rhs_func func, double error) {
 	cmlVector k1 (y0.size());
 	cmlVector k2 (y0.size());
 	cmlVector k3 (y0.size());
 	cmlVector k4 (y0.size());
+	cmlVector k5 (y0.size());
+	cmlVector k6 (y0.size());
+	cmlVector rk4 (y0.size());
+	cmlVector rk5 (y0.size());
 
-	k1 = func (t, y0);
-	k2 = func (t + 0.5 * h, y0 + h * 0.5 * k1);
-	k3 = func (t + 0.5 * h, y0 + h * 0.5 * k2);
-	k4 = func (t + h, y0 + h * k3);
+	double s = 1.;
+	double t = t0;
+	double h0 = tf - t0;
+	double h = h0;
+	double h_min = 1.0e5;
+	cmlVector h_min_y (y0);
+	cmlVector h_min_rk5 (y0);
+	cmlVector y (y0);
+	int stepcount = 0;
 
-	return y0 + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.; 
+	while (t < tf) {
+
+		k1 = h * func (t, y);
+		k2 = h * func (t + 0.25 * h, y + h * 0.25 * k1);
+		k3 = h * func (t + 3./8. * h, y + h * 3./32. * k1 + 9. / 32. * k2);
+		k4 = h * func (t + 12./13. * h, y + 1932./2197. * k1 - 7200./2197. * k2 + 7296./2197. * k3);
+		k5 = h * func (t + h, y + 439./216. * k1 - 8. * k2 + 3680./513. * k3 - 845./4104. * k4);
+		k6 = h * func (t + 0.5 * h, y - 8./27. * k1 + 2. * k2 - 3544./2565. * k3 + 1859./4104. * k4 - 11./40. * k5);
+
+		rk4 = y + 25./216. * k1 + 1408./2565. * k3 + 2197./4104. * k4 - 1./5. * k5;
+		rk5 = y + 16./135. * k1 + 6656./12825. * k3 + 28561./56430. * k4 - 9./50. * k5 + 2./55. * k6;
+
+		double error_est = cml::length(rk5 - rk4);
+
+//		cout << "error estimate = " << scientific << error_est << endl;
+
+		if (error_est > error) {
+//			s = sqrt (sqrt ( (error * h) / 2 * error_est) );
+			s = s * 0.5;
+			h = s * h;
+
+			if (h < h_min) {
+				h_min = h;
+				h_min_y = y;
+			}
+//			cout << "decreasing setting stepsize to " << s * (tf - t0) << endl;
+		} else {
+			t = t + h;
+			y = rk5;
+			stepcount ++;
+//			cout << "success" << endl;
+
+			if (1.0e1 * error_est < error ) {
+				// increasing stepsize
+//				s = sqrt (sqrt ( (error * h) / 2 * error_est) );
+				s = 2.;
+				h = s*h;
+
+//				cout << "increasing setting stepsize to " << s * (tf - t0) << " t = " << t << endl;
+			}	
+		}
+	}
+
+	cout << "used " << stepcount << " steps" << endl;
+	cout << "h_min = " << h_min << " y_state = " << h_min_y << " rk5 = " << h_min_rk5 <<  endl;
+
+	return y;
+//	return y0 + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.; 
 }
 
+cmlVector rk4_integrator (double t0, double tf, cmlVector &y0, rhs_func func, double stepsize) {
+	cmlVector k1 (y0.size());
+	cmlVector k2 (y0.size());
+	cmlVector k3 (y0.size());
+	cmlVector k4 (y0.size());
+	cmlVector y (y0);
+
+	double t = t0;
+	double h = stepsize;
+	unsigned int stepcount = 0;
+
+	if (h > tf - t0)
+		h = tf - t0;
+
+	while (t < tf) {
+		k1 = func (t, y);
+		k2 = func (t + 0.5 * h, y + h * 0.5 * k1);
+		k3 = func (t + 0.5 * h, y + h * 0.5 * k2);
+		k4 = func (t + h, y + h * k3);
+
+		t += h;
+		y = y + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.; 
+
+		if (tf - t < stepsize) {
+			h = tf - t + stepsize;
+		}
+
+		stepcount ++;
+	}
+
+	cout << "stepcount = " << stepcount << endl;
+	return y;
+}
 
 void model_init () {
 	model = new Model;
@@ -61,7 +163,6 @@ void model_init () {
 	 *  Z   |
 	 *  O---*
 	 *      Y
-	 */
 
 	body_a = Body (1., Vector3d (1., 0., 0.), Vector3d (1., 1., 1.));
 	joint_a = Joint(
@@ -86,6 +187,75 @@ void model_init () {
 			);
 
 	body_c_id = model->AddBody(body_b_id, Xtrans(Vector3d(0., 1., 0.)), joint_c, body_c);
+	 */
+
+	// base body
+	base_rot_z = Body (
+			0.,
+			Vector3d (0., 0., 0.),
+			Vector3d (0., 0., 0.)
+			);
+	joint_base_rot_z = Joint (
+			JointTypeRevolute,
+			Vector3d (0., 0., 1.)
+			);
+	base_rot_z_id = model->AddBody (0, Xtrans (Vector3d (0., 0., 0.)), joint_base_rot_z, base_rot_z);
+
+	base_rot_y = Body (
+			0.,
+			Vector3d (0., 0., 0.),
+			Vector3d (0., 0., 0.)
+			);
+	joint_base_rot_y = Joint (
+			JointTypeRevolute,
+			Vector3d (0., 1., 0.)
+			);
+	base_rot_y_id = model->AddBody (base_rot_z_id, Xtrans (Vector3d (0., 0., 0.)), joint_base_rot_y, base_rot_y);
+
+	base_rot_x = Body (
+			1.,
+			Vector3d (0., 1., 0.),
+			Vector3d (1., 1., 1.)
+			);
+	joint_base_rot_x = Joint (
+			JointTypeRevolute,
+			Vector3d (1., 0., 0.)
+			);
+	base_rot_x_id = model->AddBody (base_rot_y_id, Xtrans (Vector3d (0., 0., 0.)), joint_base_rot_x, base_rot_x);
+
+	// child body
+	child_rot_z = Body (
+			0.,
+			Vector3d (0., 0., 0.),
+			Vector3d (0., 0., 0.)
+			);
+	joint_child_rot_z = Joint (
+			JointTypeRevolute,
+			Vector3d (0., 0., 1.)
+			);
+	child_rot_z_id = model->AddBody (base_rot_x_id, Xtrans (Vector3d (1., 0., 0.)), joint_child_rot_z, child_rot_z);
+
+	child_rot_y = Body (
+			0.,
+			Vector3d (0., 0., 0.),
+			Vector3d (0., 0., 0.)
+			);
+	joint_child_rot_y = Joint (
+			JointTypeRevolute,
+			Vector3d (0., 1., 0.)
+			);
+	child_rot_y_id = model->AddBody (child_rot_z_id, Xtrans (Vector3d (0., 0., 0.)), joint_child_rot_y, child_rot_y);
+
+	child_rot_x = Body (
+			1.,
+			Vector3d (0., 1., 0.),
+			Vector3d (1., 1., 1.)
+			);
+	joint_child_rot_x = Joint (
+			JointTypeRevolute,
+			Vector3d (1., 0., 0.)
+			);
+	child_rot_x_id = model->AddBody (child_rot_y_id, Xtrans (Vector3d (0., 0., 0.)), joint_child_rot_x, child_rot_x);
 
 	Q = cmlVector(model->mBodies.size() - 1);
 	QDot = cmlVector(model->mBodies.size() - 1);
@@ -97,7 +267,10 @@ void model_init () {
 	QDDot.zero();
 	Tau.zero();
 
-	contact_body_id = body_c_id;
+	Q[0] = 0.1;
+	Q[2] = 0.1;
+
+	contact_body_id = child_rot_x_id;
 	contact_point.set (1., 0., 0.);
 	contact_normal.set (0., 1., 0.);
 
@@ -105,7 +278,8 @@ void model_init () {
 
 	// we call model_update once to update the internal variables for the
 	// state, etc.
-	model_update (0.);
+	ForwardDynamicsContacts (*model, Q, QDot, Tau, QDDot);
+
 }
 
 cmlVector rhs_contact (double t, const cmlVector &y) {
@@ -156,7 +330,6 @@ cmlVector rhs_normal (double t, const cmlVector &y) {
 	return res;
 }
 
-
 void model_update_contact (double delta_time) {
 	unsigned int size = Q.size();
 	unsigned int i;
@@ -169,7 +342,7 @@ void model_update_contact (double delta_time) {
 	}
 
 	cmlVector ynew (size * 2);
-	ynew = rk4_integrator (0., delta_time, y, rhs_contact);
+	ynew = rk4_integrator (0., delta_time, y, rhs_contact, delta_time);
 
 	for (i = 0; i < size; i++) {
 		Q[i] += ynew[i];
@@ -204,12 +377,15 @@ void model_update (double delta_time) {
 	}
 
 	cmlVector ynew (size * 2);
-	ynew = rk4_integrator (0., delta_time, y, rhs_normal);
+//	ynew = rk45_integrator (0., delta_time, y, rhs_normal, 1.0e-3);
+	ynew = rk4_integrator (0., delta_time, y, rhs_normal, delta_time);
 
 	for (i = 0; i < size; i++) {
 		Q[i] = ynew[i];
 		QDot[i] = ynew[i + size];
 	}
+
+	cout << "y = " << ynew << endl;
 }
 
 Model* model_get() {
