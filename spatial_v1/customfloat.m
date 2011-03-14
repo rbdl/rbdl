ead99c43d5292ea22ba6be1beadaeb82ec76f701
Martin Felis <martin@fysx.org>
2011-03-14 16:26:00 +0100

    minor changes to Featherstones spatial_v1

--- spatial_v1/customfloat.m
+++ spatial_v1/customfloat.m
@@ -1,6 +1,6 @@
 function [afb, qdd] = customfloat()
 
-nb = 2;
+nb = 1;
 bf = 0;
 skew = 0;
 taper = 0;
@@ -16,18 +16,18 @@ model.parent(i) = 0;
 model.Xtree{i} = Xtrans([0 0 0]);
 model.jaxis{i} = [0 0 1]';
 mass = 1.;
-CoM = [1 0 0];
+CoM = [0 1 0];
 Icm = diag([1 1 1]);
 model.I{i} = mcI(mass, CoM, Icm);
 
-i = 2;
-model.parent(i) = 1;
-model.Xtree{i} = Xtrans([2 0 0]);
-model.jaxis{i} = [0 0 1]';
-mass = 1.;
-CoM = [1 0 0];
-Icm = diag([1 1 1]);
-model.I{i} = mcI(mass, CoM, Icm);
+% i = 2;
+% model.parent(i) = 1;
+% model.Xtree{i} = Xtrans([2 0 0]);
+% model.jaxis{i} = [0 0 1]';
+% mass = 1.;
+% CoM = [1 0 0];
+% Icm = diag([1 1 1]);
+% model.I{i} = mcI(mass, CoM, Icm);
 
 floatmodel = floatbase (model);
 
@@ -39,6 +39,6 @@ X_B = eye(6);
 v_B = zeros(6,1);
 
 tau(1) = 1;
-
+v_B = [0 0 -1 1 0 0]';
 [afb, qdd] = FDf (floatmodel, X_B, v_B, q, qd, tau, {}, [0; -9.81; 0]);
 
