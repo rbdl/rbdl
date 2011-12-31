12071272aa630c34db3d6e1561c38dab71c08b1d
Martin Felis <martin@fysx.org>
2011-12-31 11:15:48 +0100

    updated documentation for proper linking of the example

--- doc/example.h
+++ doc/example.h
@@ -5,19 +5,21 @@
  * compute the forward dynamics for it:
  *
  * \include example.cc
- *
+ * 
  * If the library itself is already created, one can compile this example
- * with:
- * \code
- * 	g++ example.cc -I<path to src folder> -lrbdl -L<path to librbdl.a> -o example
- * \endcode
- *
- * Additionally there is a CMakeLists.txt, that can be used to automatically
- * create the makefiles by using <a href="http://www.cmake.org">CMake</a>.
- * It uses the script FindRBDL.cmake which can be used to find the library
- * and include directory of the headers.
+ * with CMake. In the example folder is a CMakeLists.txt file, that can be
+ * used to automatically create the makefiles by using <a
+ * href="http://www.cmake.org">CMake</a>.  It uses the script
+ * FindRBDL.cmake which can be used to find the library and include
+ * directory of the headers.
  *
  * The FindRBDL.cmake script can use the environment variables RBDL_PATH,
  * RBDL_INCLUDE_PATH, and RBDL_LIBRARY_PATH to find the required files.
+ *
+ * To build it manually you have to specify the following compiler and
+ * linking switches:
+ * \code
+ * 	g++ example.cc -I<path to build folder>/src -I<path to src folder> -lrbdl -L<path to librbdl.a> -o example
+ * \endcode
  *
  */
