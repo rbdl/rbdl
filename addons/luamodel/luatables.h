2aa27573b192e5d7893689358da9c5f1077115e3
Martin Felis <martin.felis@iwr.uni-heidelberg.de>
2013-01-22 15:51:17 +0100

    updated to newest luatables also in the default branch

--- addons/luamodel/luatables.h
+++ addons/luamodel/luatables.h
@@ -29,27 +29,33 @@
 #include <vector>
 #include <string>
 
-extern "C" {
-#include <lua.h>
+extern "C"
+{
+   #include <lua.h>
+   #include <lauxlib.h>
+   #include <lualib.h>
 }
 
 bool ltOpenFile (lua_State **L, const char *filename);
 void ltClose (lua_State **L);
 
-std::string ltGetStringAt (lua_State *L, const char *path_str, int index, const std::string &default_result = "");
+std::string ltGetStringAt (lua_State *L, const char *path_str, const int index, const std::string &default_result = "");
 std::string ltGetString (lua_State *L, const char *path_str, const std::string &default_result = "");
 
-double ltGetDoubleAt (lua_State *L, const char *path_str, int index, const double &default_result = 0.);
+double ltGetDoubleAt (lua_State *L, const char *path_str, const int index, const double &default_result = 0.);
 double ltGetDouble (lua_State *L, const char *path_str, const double &default_result = 0.);
 
-size_t ltGetLengthAt (lua_State *L, const char *path_str, int index);
+size_t ltGetLengthAt (lua_State *L, const char *path_str, const int index);
 size_t ltGetLength (lua_State *L, const char *path_str);
 
-std::vector<std::string> ltGetKeysAt (lua_State *L, const char *path_str, int index);
+std::vector<std::string> ltGetKeysAt (lua_State *L, const char *path_str, const int index);
 std::vector<std::string> ltGetKeys (lua_State *L, const char *path_str);
 
-std::vector<double> ltGetDoubleArrayAt (lua_State *L, const char *path_str, int index);
-std::vector<double> ltGetDoubleArray (lua_State *L, const char *path_str); 
+std::vector<double> ltGetDoubleVectorAt (lua_State *L, const char *path_str, const int index);
+std::vector<double> ltGetDoubleVector (lua_State *L, const char *path_str); 
+
+bool ltGetDoubleArrayAt (lua_State *L, const char *path_str, const unsigned int count, double *dest, const int index);
+bool ltGetDoubleArray (lua_State *L, const char *path_str, const unsigned int count, double *dest); 
 
 bool ltIsNumber (lua_State *L, const char *path_str);
 bool ltIsNumberAt (lua_State *L, const char *path_str, int index);
