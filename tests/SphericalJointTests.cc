ed2a112927d63465a9cde0d6e0d75ae48dc928c7
Martin Felis <martin.felis@iwr.uni-heidelberg.de>
2013-10-15 00:57:51 +0200

    found more failing tests (I guess that is a good thing from a certain perspective...)

--- tests/SphericalJointTests.cc
+++ tests/SphericalJointTests.cc
@@ -34,10 +34,12 @@ struct SphericalJoint {
 
 		joint_rot_y = Joint (SpatialVector (0., 1., 0., 0., 0., 0.));
 
-		emu_body_id = emulated_model.AddBody (0, Xtrans (Vector3d (0., 0., 0.)), joint_rot_zyx, body);
+		emulated_model.AppendBody (Xtrans(Vector3d (0., 0., 0.)), joint_rot_y, body);
+		emu_body_id = emulated_model.AppendBody (Xtrans (Vector3d (1., 0., 0.)), joint_rot_zyx, body);
 		emu_child_id = emulated_model.AppendBody (Xtrans (Vector3d (1., 0., 0.)), joint_rot_y, body);
 
-		sph_body_id = spherical_model.AddBody (0, Xtrans (Vector3d (0., 0., 0.)), joint_spherical, body);
+		spherical_model.AppendBody (Xtrans(Vector3d (0., 0., 0.)), joint_rot_y, body);
+		sph_body_id = spherical_model.AppendBody (Xtrans (Vector3d (1., 0., 0.)), joint_spherical, body);
 		sph_child_id = spherical_model.AppendBody (Xtrans (Vector3d (1., 0., 0.)), joint_rot_y, body);
 
 		emuQ = VectorNd::Zero ((size_t) emulated_model.q_size);
@@ -94,16 +96,18 @@ struct SphericalJoint {
 	VectorNd sphTau;
 };
 
+
 TEST_FIXTURE(SphericalJoint, TestQIndices) {
 	CHECK_EQUAL (0, spherical_model.mJoints[1].q_index);
-	CHECK_EQUAL (3, spherical_model.mJoints[2].q_index);
+	CHECK_EQUAL (1, spherical_model.mJoints[2].q_index);
+	CHECK_EQUAL (4, spherical_model.mJoints[3].q_index);
 
-	CHECK_EQUAL (4, emulated_model.q_size);
-	CHECK_EQUAL (4, emulated_model.qdot_size);
+	CHECK_EQUAL (5, emulated_model.q_size);
+	CHECK_EQUAL (5, emulated_model.qdot_size);
 
-	CHECK_EQUAL (5, spherical_model.q_size);
-	CHECK_EQUAL (4, spherical_model.qdot_size);
-	CHECK_EQUAL (4, spherical_model.spherical_w_index[1]);
+	CHECK_EQUAL (6, spherical_model.q_size);
+	CHECK_EQUAL (5, spherical_model.qdot_size);
+	CHECK_EQUAL (5, spherical_model.spherical_w_index[2]);
 }
 
 TEST_FIXTURE(SphericalJoint, TestGetQuaternion) {
@@ -114,32 +118,34 @@ TEST_FIXTURE(SphericalJoint, TestGetQuaternion) {
 	sphQDDot = VectorNd::Zero ((size_t) spherical_model.qdot_size);
 	sphTau = VectorNd::Zero ((size_t) spherical_model.qdot_size);
 
-	CHECK_EQUAL (9, spherical_model.q_size);
-	CHECK_EQUAL (7, spherical_model.qdot_size);
+	CHECK_EQUAL (10, spherical_model.q_size);
+	CHECK_EQUAL (8, spherical_model.qdot_size);
 
 	CHECK_EQUAL (0, spherical_model.mJoints[1].q_index);
-	CHECK_EQUAL (3, spherical_model.mJoints[2].q_index);
+	CHECK_EQUAL (1, spherical_model.mJoints[2].q_index);
 	CHECK_EQUAL (4, spherical_model.mJoints[3].q_index);
+	CHECK_EQUAL (5, spherical_model.mJoints[4].q_index);
 
-	CHECK_EQUAL (7, spherical_model.spherical_w_index[1]);
-	CHECK_EQUAL (8, spherical_model.spherical_w_index[3]);
+	CHECK_EQUAL (8, spherical_model.spherical_w_index[2]);
+	CHECK_EQUAL (9, spherical_model.spherical_w_index[4]);
 
-	sphQ[0] = 0.;
-	sphQ[1] = 1.;
-	sphQ[2] = 2.;
-	sphQ[3] = 3.;
-	sphQ[4] = -6.;
-	sphQ[5] = -7.;
-	sphQ[6] = -8.;
-	sphQ[7] = 4.;
-	sphQ[8] = -9.;
+	sphQ[0] = 100.;
+	sphQ[1] = 0.;
+	sphQ[2] = 1.;
+	sphQ[3] = 2.;
+	sphQ[4] = 100.;
+	sphQ[5] = -6.;
+	sphQ[6] = -7.;
+	sphQ[7] = -8;
+	sphQ[8] = 4.;
+	sphQ[9] = -9.;
 
 	Quaternion reference_1 (0., 1., 2., 4.);
-	Quaternion quat_1 = spherical_model.GetQuaternion (1, sphQ);
+	Quaternion quat_1 = spherical_model.GetQuaternion (2, sphQ);
 	CHECK_ARRAY_EQUAL (reference_1.data(), quat_1.data(), 4);
 
 	Quaternion reference_3 (-6., -7., -8., -9.);
-	Quaternion quat_3 = spherical_model.GetQuaternion (3, sphQ);
+	Quaternion quat_3 = spherical_model.GetQuaternion (4, sphQ);
 	CHECK_ARRAY_EQUAL (reference_3.data(), quat_3.data(), 4);
 }
 
@@ -152,13 +158,13 @@ TEST_FIXTURE(SphericalJoint, TestSetQuaternion) {
 	sphTau = VectorNd::Zero ((size_t) spherical_model.qdot_size);
 
 	Quaternion reference_1 (0., 1., 2., 3.);
-	spherical_model.SetQuaternion (1, reference_1, sphQ);
-	Quaternion test = spherical_model.GetQuaternion (1, sphQ);
+	spherical_model.SetQuaternion (2, reference_1, sphQ);
+	Quaternion test = spherical_model.GetQuaternion (2, sphQ);
 	CHECK_ARRAY_EQUAL (reference_1.data(), test.data(), 4);
 
 	Quaternion reference_2 (11., 22., 33., 44.);
-	spherical_model.SetQuaternion (3, reference_2, sphQ);
-	test = spherical_model.GetQuaternion (3, sphQ);
+	spherical_model.SetQuaternion (4, reference_2, sphQ);
+	test = spherical_model.GetQuaternion (4, sphQ);
 	CHECK_ARRAY_EQUAL (reference_2.data(), test.data(), 4);
 }
 
@@ -175,7 +181,7 @@ TEST_FIXTURE(SphericalJoint, TestOrientation) {
 	Quaternion quat =  Quaternion::fromAxisAngle (Vector3d (1., 0., 0.), emuQ[2]) 
 		* Quaternion::fromAxisAngle (Vector3d (0., 1., 0.), emuQ[1])
 		* Quaternion::fromAxisAngle (Vector3d (0., 0., 1.), emuQ[0]);
-	spherical_model.SetQuaternion (1, quat, sphQ);
+	spherical_model.SetQuaternion (2, quat, sphQ);
 
 	Matrix3d emu_orientation = CalcBodyWorldOrientation (emulated_model, emuQ, emu_child_id);
 	Matrix3d sph_orientation = CalcBodyWorldOrientation (spherical_model, sphQ, sph_child_id);
@@ -221,4 +227,34 @@ TEST_FIXTURE(SphericalJoint, TestForwardDynamicsQAndQDot) {
 	CHECK_ARRAY_CLOSE (emulated_model.a[emu_child_id].data(), spherical_model.a[sph_child_id].data(), 6, TEST_PREC);
 }
 
+TEST_FIXTURE(SphericalJoint, TestForwardDynamicsQAndQDotAndTau ) {
+	emuQ[0] = 1.1;
+	emuQ[1] = 1.1;
+	emuQ[2] = 1.1;
+	emuQ[3] = 1.1;
 
+	emuQDot[0] = 2.2;
+	emuQDot[1] = 2.2;
+	emuQDot[2] = 2.2;
+	emuQDot[3] = 2.2;
+
+	emuTau[0] = 1.;
+	emuTau[1] = 1.;
+	emuTau[2] = 1.;
+	emuTau[3] = 1.;
+
+	for (unsigned int i = 0; i < emuTau.size(); i++) {
+		sphTau[i] = emuTau[i];
+	}
+
+	ConvertQAndQDotFromEmulated (emulated_model, emuQ, emuQDot, spherical_model, &sphQ, &sphQDot);
+	ConvertQAndQDotFromEmulated (emulated_model, emuQ, emuTau, spherical_model, &sphQ, &sphTau);
+
+	ClearLogOutput();
+	ForwardDynamics (emulated_model, emuQ, emuQDot, emuTau, emuQDDot);
+	ForwardDynamics (spherical_model, sphQ, sphQDot, sphTau, sphQDDot);
+
+	cout << LogOutput.str() << endl;
+
+	CHECK_ARRAY_CLOSE (emulated_model.a[emu_child_id].data(), spherical_model.a[sph_child_id].data(), 6, TEST_PREC);
+}
