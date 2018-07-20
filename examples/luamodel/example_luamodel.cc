152fffd9735eb77516a082448d42b1ee61ac7aad
Martin Felis <martin@fysx.org>
2018-07-20 16:48:24 +0200

    Improved urdfreader and luamodel examples to have more output and to use user provided model file instead of hardcoded file name
    --HG--
    branch : next

--- examples/luamodel/example_luamodel.cc
+++ examples/luamodel/example_luamodel.cc
@@ -1,6 +1,6 @@
 /*
  * RBDL - Rigid Body Dynamics Library
- * Copyright (c) 2011-2016 Martin Felis <martin@fysx.org>
+ * Copyright (c) 2011-2018 Martin Felis <martin@fysx.org>
  *
  * Licensed under the zlib license. See LICENSE for more details.
  */
@@ -8,9 +8,10 @@
 #include <iostream>
 
 #include <rbdl/rbdl.h>
+#include <rbdl/rbdl_utils.h>
 
 #ifndef RBDL_BUILD_ADDON_LUAMODEL
-	#error "Error: RBDL addon LuaModel not enabled."
+#error "Error: RBDL addon LuaModel not enabled."
 #endif
 
 #include <rbdl/addons/luamodel/luamodel.h>
@@ -19,29 +20,41 @@ using namespace RigidBodyDynamics;
 using namespace RigidBodyDynamics::Math;
 
 int main (int argc, char* argv[]) {
-	rbdl_check_api_version (RBDL_API_VERSION);
+  rbdl_check_api_version (RBDL_API_VERSION);
 
-	Model* model = NULL;
+  Model* model = NULL;
 
-	model = new Model();
+  model = new Model();
 
-	if (!Addons::LuaModelReadFromFile ("./samplemodel.lua", model, false)) {
-		std::cerr << "Error loading model ./samplemodel.lua" << std::endl;
-		abort();
-	}
+  if (argc != 2) {
+    std::cerr << "Error: Invalid number of arguments." << std::endl;
+    std::cerr << "usage: " << argv[0] << " <model.lua>" << std::endl;
+    exit(-1);
+  }
 
-	VectorNd Q = VectorNd::Zero (model->dof_count);
-	VectorNd QDot = VectorNd::Zero (model->dof_count);
-	VectorNd Tau = VectorNd::Zero (model->dof_count);
-	VectorNd QDDot = VectorNd::Zero (model->dof_count);
+  if (!Addons::LuaModelReadFromFile (argv[1], model, false)) {
+    std::cerr << "Error loading model " << argv[1] << std::endl;
+    abort();
+  }
 
- 	ForwardDynamics (*model, Q, QDot, Tau, QDDot);
+  std::cout << "Degree of freedom overview:" << std::endl;
+  std::cout << Utils::GetModelDOFOverview(*model);
 
-	std::cout << Q.transpose() << std::endl;
-	std::cout << QDDot.transpose() << std::endl;
+  std::cout << "Model Hierarchy:" << std::endl;
+  std::cout << Utils::GetModelHierarchy(*model);
 
-	delete model;
+  VectorNd Q = VectorNd::Zero (model->dof_count);
+  VectorNd QDot = VectorNd::Zero (model->dof_count);
+  VectorNd Tau = VectorNd::Zero (model->dof_count);
+  VectorNd QDDot = VectorNd::Zero (model->dof_count);
 
- 	return 0;
+  std::cout << "Forward Dynamics with q, qdot, tau set to zero:" << std::endl;
+  ForwardDynamics (*model, Q, QDot, Tau, QDDot);
+
+  std::cout << QDDot.transpose() << std::endl;
+
+  delete model;
+
+  return 0;
 }
 
--- examples/urdfreader/example_urdfreader.cc
+++ examples/urdfreader/example_urdfreader.cc
@@ -8,9 +8,10 @@
 #include <iostream>
 
 #include <rbdl/rbdl.h>
+#include <rbdl/rbdl_utils.h>
 
 #ifndef RBDL_BUILD_ADDON_URDFREADER
-	#error "Error: RBDL addon URDFReader not enabled."
+#error "Error: RBDL addon URDFReader not enabled."
 #endif
 
 #include <rbdl/addons/urdfreader/urdfreader.h>
@@ -19,27 +20,39 @@ using namespace RigidBodyDynamics;
 using namespace RigidBodyDynamics::Math;
 
 int main (int argc, char* argv[]) {
-	rbdl_check_api_version (RBDL_API_VERSION);
+  rbdl_check_api_version (RBDL_API_VERSION);
 
-	Model* model = new Model();
+  Model* model = new Model();
 
-	if (!Addons::URDFReadFromFile ("./samplemodel.urdf", model, false)) {
-		std::cerr << "Error loading model ./samplemodel.urdf" << std::endl;
-		abort();
-	}
+  if (argc != 2) {
+    std::cerr << "Error: not right number of arguments." << std::endl;
+    std::cerr << "usage: " << argv[0] << " <model.urdf>" << std::endl;
+    exit(-1);
+  }
 
-	VectorNd Q = VectorNd::Zero (model->dof_count);
-	VectorNd QDot = VectorNd::Zero (model->dof_count);
-	VectorNd Tau = VectorNd::Zero (model->dof_count);
-	VectorNd QDDot = VectorNd::Zero (model->dof_count);
+  if (!Addons::URDFReadFromFile (argv[1], model, false)) {
+    std::cerr << "Error loading model " << argv[1] << std::endl;
+    abort();
+  }
 
- 	ForwardDynamics (*model, Q, QDot, Tau, QDDot);
+  std::cout << "Degree of freedom overview:" << std::endl;
+  std::cout << Utils::GetModelDOFOverview(*model);
 
-	std::cout << Q.transpose() << std::endl;
-	std::cout << QDDot.transpose() << std::endl;
+  std::cout << "Model Hierarchy:" << std::endl;
+  std::cout << Utils::GetModelHierarchy(*model);
 
-	delete model;
+  VectorNd Q = VectorNd::Zero (model->q_size);
+  VectorNd QDot = VectorNd::Zero (model->qdot_size);
+  VectorNd Tau = VectorNd::Zero (model->qdot_size);
+  VectorNd QDDot = VectorNd::Zero (model->qdot_size);
 
- 	return 0;
+  std::cout << "Forward Dynamics with q, qdot, tau set to zero:" << std::endl;
+  ForwardDynamics (*model, Q, QDot, Tau, QDDot);
+
+  std::cout << QDDot.transpose() << std::endl;
+
+  delete model;
+
+  return 0;
 }
 
