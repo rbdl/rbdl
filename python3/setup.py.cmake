fdb6d4560cea13d5de354e7c6aef51ca420e4ab1
Felix Richter <judge@felixrichter.tech>
2018-09-21 22:16:21 +0200

    created working python3 extention builder -> need to fix python 3 specific issues still

--- python3/setup.py.cmake
+++ python3/setup.py.cmake
@@ -2,58 +2,28 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
+from distutils.sysconfig import get_python_lib
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
 import os
+import sys
 import numpy as np
 
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
+if not os.path.exists('rbdl.so'):
+	print("""The setup.py script should be executed from the build directory.""")
+	sys.exit(1)
 
-extra_params = {}
-extra_params['include_dirs'] = [
-    '/usr/include',
-    BASEDIR,
-    np.get_include(),
-    '@RBDL_INCLUDE_DIR@',
-    '@EIGEN3_INCLUDE_DIR@',
-    '@CMAKE_CURRENT_SOURCE_DIR@',
-    '@RBDL_SOURCE_DIR@/include',
-    '@RBDL_BINARY_DIR@/include',
-    '@PROJECT_SOURCE_DIR@',
-    '/usr/include/eigen3/'
-]
-
-extra_params['language'] = 'c++'
-extra_params['extra_compile_args'] = ["-O3", "-Wno-unused-variable", "-std=c++11"]
-extra_params['libraries'] = ['rbdl']
-extra_params['library_dirs'] = [
-  '${CMAKE_CURRENT_BINARY_DIR}/../',
-  '${CMAKE_INSTALL_PREFIX}/lib/',
-  '/usr/lib',
-  BASEDIR
-  ]
-extra_params['extra_link_args'] = [
-  "-Wl,-O1",
-  "-Wl,--as-needed", 
-  ]
-
-if os.name == 'posix':
-    extra_params['runtime_library_dirs'] = extra_params['library_dirs']
-
-ext_modules = [
-    Extension("rbdl",  ["crbdl.pxd", "rbdl.pyx"], **extra_params),
-]
+lib_path = get_python_lib()[5:]
 
 setup(
-    name='rbdl',
-    author='Martin Felis',
-    author_email='martin@fysx.org',
-    description='Python wrapper for RBDL - the Rigid Body Dynamics Library',
-    install_requires=['numpy', "cython"],
-    license='zlib',
-    version='${RBDL_VERSION_MAJOR}.${RBDL_VERSION_MINOR}.${RBDL_VERSION_PATCH}',
-    url='http://rbdl.bitbucket.org/',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize(ext_modules),
+	name='rbdl',
+	author='Felix Richter',
+	author_email='orb@felixrichter.tech',
+	description='Python wrapper for RBDL - the Rigid Body Dynamics Library',
+	license='zlib',
+	version='${RBDL_VERSION_MAJOR}.${RBDL_VERSION_MINOR}.${RBDL_VERSION_PATCH}',
+	url='http://rbdl.bitbucket.org/',
+	data_files = [(lib_path, ["rbdl.so"])],
 )
+
