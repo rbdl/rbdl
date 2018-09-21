8f4e933417529184842c8cb2d000fabec5a8f4c5
Felix Richter <judge@felixrichter.tech>
2018-09-21 22:03:25 +0200

    add robustnes regarding differnt install locations

--- python/setup.py.cmake
+++ python/setup.py.cmake
@@ -10,46 +10,11 @@ import os
 import sys
 import numpy as np
 
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
-
-#extra_params = {}
-#extra_params['include_dirs'] = [
-#    '/usr/include',
-#    BASEDIR,
-#    np.get_include(),
-#    '@RBDL_INCLUDE_DIR@',
-#    '@EIGEN3_INCLUDE_DIR@',
-#    '@CMAKE_CURRENT_SOURCE_DIR@',
-#    '@RBDL_SOURCE_DIR@/include',
-#    '@RBDL_BINARY_DIR@/include',
-#    '@PROJECT_SOURCE_DIR@',
-#    '/usr/include/eigen3/'
-#]
-#
-#extra_params['language'] = 'c++'
-#extra_params['extra_compile_args'] = ["-O3", "-Wno-unused-variable", "-std=c++11"]
-#extra_params['libraries'] = ['rbdl']
-#extra_params['library_dirs'] = [
-#  '${CMAKE_CURRENT_BINARY_DIR}/../',
-#  '${CMAKE_INSTALL_PREFIX}/lib/',
-#  '/usr/lib',
-#  BASEDIR
-#  ]
-#extra_params['extra_link_args'] = [
-#  "-Wl,-O1",
-#  "-Wl,--as-needed", 
-#  ]
-
-#if os.name == 'posix':
-#    extra_params['runtime_library_dirs'] = extra_params['library_dirs']
-#
-#ext_modules = [
-#    Extension("rbdl",  ["crbdl.pxd", "rbdl.pyx"], **extra_params),
-#]
-#
 if not os.path.exists('rbdl.so'):
 	print("""The setup.py script should be executed from the build directory.""")
 	sys.exit(1)
+
+lib_path = get_python_lib()[5:]
 
 setup(
 	name='rbdl',
@@ -59,7 +24,5 @@ setup(
 	license='zlib',
 	version='${RBDL_VERSION_MAJOR}.${RBDL_VERSION_MINOR}.${RBDL_VERSION_PATCH}',
 	url='http://rbdl.bitbucket.org/',
-	data_files = [('lib/python2.7/site-packages/', ["rbdl.so"])],
-	#cmdclass={'build_ext': build_ext},
-	#ext_modules=cythonize(ext_modules),
+	data_files = [(lib_path, ["rbdl.so"])],
 )
