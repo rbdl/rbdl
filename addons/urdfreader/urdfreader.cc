4bd14a9301fc5d77db165f8a772c105300cee990
Martin Felis <martin@fysx.org>
2018-12-08 21:48:29 +0100

    Removed special treatment of base_link body.

--- addons/urdfreader/urdfreader.cc
+++ addons/urdfreader/urdfreader.cc
@@ -196,9 +196,7 @@ void construct_model(Model *rbdl_model, ModelPtr urdf_model,
     // determine where to add the current joint and child body
     unsigned int rbdl_parent_id = 0;
 
-    if (urdf_parent->name != "base_link") {
-      rbdl_parent_id = rbdl_model->GetBodyId(urdf_parent->name.c_str());
-    }
+    rbdl_parent_id = rbdl_model->GetBodyId (urdf_parent->name.c_str());
 
     if (rbdl_parent_id == std::numeric_limits<unsigned int>::max()) {
       ostringstream error_msg;
