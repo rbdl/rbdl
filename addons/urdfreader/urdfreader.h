f3c0b7e7cd735912aa149cbf4f8d243f39646200
ju6ge <judge@felixrichter.tech>
2021-03-30 17:34:52 +0200

    add doxygen to urdf loading functions

--- addons/urdfreader/urdfreader.h
+++ addons/urdfreader/urdfreader.h
@@ -8,9 +8,27 @@ namespace RigidBodyDynamics {
 struct Model;
 
 namespace Addons {
+  /**
+   * This function will load a URDF model from a file.
+   *
+   * @param filename: path to the URDF file
+   * @param model: reference to the (loaded) multibody model
+   * @param floating_pase: does the model use a floating base
+   * @param verbose: information will be printed to the command window if this
+   *                 is set to true
+   */
   RBDL_DLLAPI bool URDFReadFromFile (const char* filename, Model* model,
                                      bool floating_base, bool verbose = false);
 
+  /**
+   * This function will load a URDF model from a c string.
+   *
+   * @param model_xml_string: URDF data in form of a string
+   * @param model: reference to the (loaded) multibody model
+   * @param floating_pase: does the model use a floating base
+   * @param verbose: information will be printed to the command window if this
+   *                 is set to true
+   */
   RBDL_DLLAPI bool URDFReadFromString (const char* model_xml_string,
                                        Model* model, bool floating_base,
                                        bool verbose = false);
