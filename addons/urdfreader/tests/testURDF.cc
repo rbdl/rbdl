7d83f2f218ca25721534613a18c8d2d8b9604dff
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2023-06-11 22:42:39 +0200

    added partial URDF generation test case for a choosen root link

--- addons/urdfreader/tests/testURDF.cc
+++ addons/urdfreader/tests/testURDF.cc
@@ -52,12 +52,14 @@ TEST_CASE(__FILE__"_CenterOfMass", ""){
   Vector3d model_com;
   double mass;
   RigidBodyDynamics::Utils::CalcCenterOfMass (model, q_zero, qdot_zero,
-					      NULL, mass, model_com);
+                          NULL, mass, model_com);
   CHECK_THAT(model_com[0], IsClose(0.2,TEST_PREC,TEST_PREC));
   CHECK_THAT(model_com[1], IsClose(0.0945087,TEST_PREC,TEST_PREC));
   CHECK_THAT(model_com[2], IsClose(-0.0597335,TEST_PREC,TEST_PREC));
 
   CHECK_THAT(mass, IsClose(1.5,TEST_PREC, TEST_PREC));
+  // Check that the root link (1st link) has the weight of the fixed links
+  CHECK_THAT(model.mBodies.front().mMass, IsClose(0.01,TEST_PREC, TEST_PREC));
 }
 
 TEST_CASE(__FILE__"_CenterOfMass_WithPartial", ""){
@@ -71,6 +73,7 @@ TEST_CASE(__FILE__"_CenterOfMass_WithPartial", ""){
                                              false, false);
   CHECK(model.q_size == 7);
   CHECK(model.qdot_size == 7);
+  CHECK(model.mBodies.size() == 8);
   VectorNd q_zero (VectorNd::Zero (model.q_size));
   VectorNd qdot_zero (VectorNd::Zero (model.qdot_size));
   RigidBodyDynamics::UpdateKinematics (model, q_zero, qdot_zero, qdot_zero);
@@ -91,4 +94,57 @@ TEST_CASE(__FILE__"_CenterOfMass_WithPartial", ""){
   CHECK_THAT(model_com[2], IsClose(-0.1992383036,TEST_PREC,TEST_PREC));
 
   CHECK_THAT(mass, IsClose(8.035202,TEST_PREC, TEST_PREC));
+
+  // Only retrieve the part of the gripper
+  Model gripper_model;
+  bool gripperModelLoaded = PartialURDFReadFromFile(modelFile.c_str(),
+                                                    &gripper_model,
+                                                    "arm_left_tool_link",
+                                                    {},
+                                                    false);
+  CHECK(gripperModelLoaded);
+  CHECK(gripper_model.mFixedBodies.size()  == 9);
+  CHECK(gripper_model.mBodies.size() == 3);
+  double real_mass = 0;
+  for(auto body :  gripper_model.mBodies)
+      real_mass +=  body.mMass;
+  for(auto body :  gripper_model.mFixedBodies)
+      real_mass +=  body.mMass;
+  real_mass -= gripper_model.mBodies.front().mMass;
+  std::cerr << "Real mass is : " << real_mass << std::endl;
+  for(auto body :  gripper_model.mBodyNameMap)
+      std::cerr <<  body.first << std::endl;
+  CHECK(gripper_model.q_size == 2);
+  CHECK(gripper_model.qdot_size == 2);
+  VectorNd q_gripper_zero (VectorNd::Zero (model.q_size));
+  VectorNd qdot_gripper_zero (VectorNd::Zero (model.qdot_size));
+  RigidBodyDynamics::UpdateKinematics (gripper_model, q_gripper_zero,
+                                       qdot_gripper_zero ,qdot_gripper_zero);
+
+  SpatialRigidBodyInertia rbi_gripper =
+      gripper_model.X_base[1].apply(gripper_model.I[1]);
+  Vector3d gripper_com = rbi_gripper.h / rbi_gripper.m;
+  CHECK_THAT(gripper_com.transpose()[0], IsClose(0.12101,TEST_PREC,TEST_PREC));
+  CHECK_THAT(gripper_com.transpose()[1], IsClose(-0.0107,TEST_PREC,TEST_PREC));
+  CHECK_THAT(gripper_com.transpose()[2], IsClose(0.03504,TEST_PREC,TEST_PREC));
+
+  Vector3d gripper_model_com;
+  double gripper_mass;
+  RigidBodyDynamics::Utils::CalcCenterOfMass (gripper_model, q_gripper_zero,
+                                              qdot_gripper_zero, NULL,
+                                              gripper_mass,
+                                              gripper_model_com);
+
+  /// @todo fix the gripper model com and gripper mass computation to also
+  /// include the fixed body links
+  CHECK_THAT(gripper_model_com[0], IsClose(0.151588,TEST_PREC,TEST_PREC));
+  CHECK_THAT(gripper_model_com[1], IsClose(-0.010766,TEST_PREC,TEST_PREC));
+  CHECK_THAT(gripper_model_com[2], IsClose(0.00447,TEST_PREC,TEST_PREC));
+
+  // It should be around 1.12272 Kg, but right now it only uses the non-fixed
+  // bodies
+  CHECK_THAT(gripper_mass, IsClose(0.21996,TEST_PREC, TEST_PREC));
+  // Check that the root link (1st link) has the weight of the fixed links
+  CHECK_THAT(gripper_model.mBodies.front().mMass, IsClose(0.90276,TEST_PREC,
+                                                          TEST_PREC));
 }
